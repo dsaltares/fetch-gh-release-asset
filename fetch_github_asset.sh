1276db4dd858588f9299449edee34147972731de
Joseph Flinn <joseph.s.flinn@gmail.com>
2021-01-27 16:38:43 +0000

    initial work on target desitnation

--- fetch_github_asset.sh
+++ fetch_github_asset.sh
@@ -12,7 +12,13 @@ fi
 
 REPO=$GITHUB_REPOSITORY
 if ! [[ -z ${INPUT_REPO} ]]; then
-  REPO=$INPUT_REPO ;
+  REPO=$INPUT_REPO
+fi
+
+# Optional target file path
+TARGET=$INPUT_FILE
+if ! [[ -z ${INPUT_TARGET} ]]; then
+  TARGET=$INPUT_TARGET
 fi
 
 # Optional personal access token for external repository
@@ -23,6 +29,19 @@ fi
 
 API_URL="https://$TOKEN:@api.github.com/repos/$REPO"
 RELEASE_DATA=$(curl $API_URL/releases/${INPUT_VERSION})
+MESSAGE=$(echo $RELEASE_DATA | jq -r ".message")
+
+if [[ "$MESSAGE" == "Not Found" ]]; then
+  echo "[!] Release asset not found"
+  echo "Release data: $RELEASE_DATA"
+  echo "-----"
+  echo "repo: $REPO"
+  echo "asset: $INPUT_FILE"
+  echo "target: $TARGET"
+  echo "version: $INPUT_VERSION"
+  exit 1
+fi
+
 ASSET_ID=$(echo $RELEASE_DATA | jq -r ".assets | map(select(.name == \"${INPUT_FILE}\"))[0].id")
 TAG_VERSION=$(echo $RELEASE_DATA | jq -r ".tag_name" | sed -e "s/^v//" | sed -e "s/^v.//")
 
@@ -36,6 +55,6 @@ curl \
   -L \
   -H "Accept: application/octet-stream" \
   "$API_URL/releases/assets/$ASSET_ID" \
-  -o ${INPUT_FILE}
+  -o ${TARGET}
 
 echo "::set-output name=version::$TAG_VERSION"
